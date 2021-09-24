2e343dfe0ea4e36aad53204654d3932b415994be
simon <brotherlogic@gmail.com>
2021-09-24 07:29:49 -0700

    Enable debug mode

--- tempapi.go
+++ tempapi.go
@@ -123,20 +123,23 @@ func (s *Server) Proc(ctx context.Context, req *pb.ProcRequest) (*pb.ProcRespons
 	ntemp.Set(float64(devices.Devices[0].Traits.TemperatureVal.Value))
 	nhumid.Set(float64(devices.Devices[0].Traits.HumidityVal.Value))
 
-	conn2, err2 := s.FDialServer(ctx, "queue")
-	if err2 != nil {
-		return nil, err2
+	var err3 error
+	if !req.GetDebug() {
+		conn2, err2 := s.FDialServer(ctx, "queue")
+		if err2 != nil {
+			return nil, err2
+		}
+		defer conn2.Close()
+		qclient := qpb.NewQueueServiceClient(conn2)
+		upup := &pb.ProcRequest{}
+		data, _ := proto.Marshal(upup)
+		_, err3 = qclient.AddQueueItem(ctx, &qpb.AddQueueItemRequest{
+			QueueName: "temp",
+			RunTime:   time.Now().Add(time.Minute).Unix(),
+			Payload:   &google_protobuf.Any{Value: data},
+			Key:       "ntemp",
+		})
 	}
-	defer conn2.Close()
-	qclient := qpb.NewQueueServiceClient(conn2)
-	upup := &pb.ProcRequest{}
-	data, _ := proto.Marshal(upup)
-	_, err3 := qclient.AddQueueItem(ctx, &qpb.AddQueueItemRequest{
-		QueueName: "temp",
-		RunTime:   time.Now().Add(time.Minute).Unix(),
-		Payload:   &google_protobuf.Any{Value: data},
-		Key:       "ntemp",
-	})
 
 	return &pb.ProcResponse{
 		NestTemperature: devices.Devices[0].Traits.TemperatureVal.Value,
